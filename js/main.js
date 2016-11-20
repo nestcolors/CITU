38d6ce329213b74d02c0a64ea643fa8d8998b52d
Severynsky <severynr@gmail.com>
2016-11-20 19:32:57 +0200

    main page markup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 $('document').ready(function(){
   console.log('ready');
+
+  $('#blog-slider').slick({
+    dots: true,
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 300,
+    responsive: [
+    {
+      breakpoint: 1025,
+      settings: {
+        slidesToShow: 2.45
+      }
+    },{
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 1.5
+      }
+    }]
+  });
+
 });
